b15890eba90d3a605c524860c344d2f63022f9e4
yapei <yapei@redhat.com>
2022-11-16 09:53:23 +0800

    add cypress tests support

---
+++ cypress/e2e/example.cy.ts
@@ -0,0 +1,27 @@
+// enables intelligent code completion for Cypress commands
+// https://on.cypress.io/intelligent-code-completion
+/// <reference types="Cypress" />
+
+context('Cypress TodoMVC test', () => {
+  beforeEach(() => {
+    // https://on.cypress.io/visit
+    cy.visit('/')
+  })
+
+  it('adds 2 todos', function () {
+    cy.get('.new-todo')
+      .type('learn testing{enter}')
+      .type('be cool{enter}')
+    cy.get('.todo-list li').should('have.length', 2)
+
+    // make sure cypress-axe is working
+    cy.injectAxe()
+  })
+
+  // more examples
+  //
+  // https://github.com/cypress-io/cypress-example-todomvc
+  // https://github.com/cypress-io/cypress-example-kitchensink
+  // https://on.cypress.io/writing-your-first-test
+})
+
